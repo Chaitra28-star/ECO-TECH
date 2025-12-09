0053beb572a1d30fac5ab1dd12389f388ec489f1
Chaitra28-star <rchaitra922@gmail.com>
2025-12-09 11:17:32 +0530

    Add files via upload

---
+++ base44Client.js
@@ -0,0 +1,13 @@
+export const base44 = {
+  entities: {
+    WasteReport: {
+      list: async () => [],
+    },
+    Transaction: {
+      filter: async () => [],
+    },
+    User: {
+      list: async () => [],
+    }
+  }
+};
